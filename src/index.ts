9bf31b7ed19a8427664ef3821246088108424129
maeda <takanori@stroly.jp>
2020-03-18 19:46:51 +0900

    とりあえず動くところまで実装

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+import * as exporessWS from 'express-ws';
+import * as ws from 'ws';
+
+const app = express();
+exporessWS(app);
+const port = process.env.PORT || 3030;
+
+const clients: ws[] = [];
+
+// @ts-ignore: ws injected via express-ws
+(app as exporessWS.Application).ws('/:room', (client, req) => {
+  clients.push(client);
+
+  client.on('message', message => {
+    console.log('Received: ' + message);
+    clients.forEach(client => {
+      client.send(message);
+    });
+  });
+
+  client.on('close', () => {
+    console.log('I lost a client');
+  });
+
+
+});
+
+app.listen(port, () => console.log(`Hello app listening on port ${port}!`));
